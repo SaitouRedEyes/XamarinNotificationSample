8825a5f947c6e570d8b6c7db7c96261fc440a6c1
SaitouRedEyes <zeca.saitou@gmail.com>
2018-07-16 19:28:43 -0300

    Xamarin Local Notification Example
    This is a example of the local push notification use on Xamarin.Android.

---
+++ NotificationSample/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Support.V4.App;
+using TaskStackBuilder = Android.Support.V4.App.TaskStackBuilder;
+
+namespace NotificationSample
+{
+    [Activity(Label = "NotificationSample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private int count;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            count = 0;
+
+            Button buttonSendNotification = FindViewById<Button>(Resource.Id.buttonSendNotification);
+
+            buttonSendNotification.Click += OnButtonSendNotificationClicked;
+        }
+
+        private void OnButtonSendNotificationClicked(object sender, System.EventArgs e)
+        {
+            count++;
+
+            // When the user clicks the notification, SecondActivity will start up.
+            Intent resultIntent = new Intent(this, typeof(ReceiveNotification));
+
+            Bundle myParameters = new Bundle();
+            myParameters.PutInt("count", count);
+
+            // Pass some values to SecondActivity:
+            resultIntent.PutExtras(myParameters);
+
+            // Construct a back stack for cross-task navigation:
+            TaskStackBuilder stackBuilder = TaskStackBuilder.Create(this);
+            stackBuilder.AddParentStack(Java.Lang.Class.FromType(typeof(ReceiveNotification)));
+            stackBuilder.AddNextIntent(resultIntent);
+
+            // Create the PendingIntent with the back stack:            
+            PendingIntent resultPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
+            
+            //Build Notification
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .SetAutoCancel(true)
+                .SetContentIntent(resultPendingIntent)
+                .SetContentTitle("Button Clicked")
+                .SetNumber(count)
+                .SetSmallIcon(Resource.Drawable.ic_stat_button_click)
+                .SetContentText(string.Format("The button has been clicked {0} times.", count));
+
+            //Send Notification
+            NotificationManager nf = (NotificationManager)GetSystemService(Context.NotificationService);
+            nf.Notify(1000, builder.Build());
+        }
+    }
+}
+
---
+++ NotificationSample/ReceiveNotification.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+
+namespace NotificationSample
+{
+    [Activity(Label = "ReceiveNotification")]
+    public class ReceiveNotification : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.receive_notification_layout);
+
+            // Get the count value passed to us from MainActivity:
+            int count = Intent.Extras.GetInt("count", -1);
+
+            // No count was passed? Then just return.
+            if (count <= 0)
+                return;
+
+            Toast.MakeText(this, count.ToString() + " Notifications", ToastLength.Short).Show();
+        }
+    }
+}
